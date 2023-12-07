6fb9eb0fdcfecf7cebf95dbed9c336bbcabfdf4c
Mike Peace <mikepeace981@gmail.com>
2023-12-07 03:54:40 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import {Route, BrowseRouter as Router, Routes} from 'react-router-dom'
+import Navbar from './Components/Navbar'
+
+const App = () => {
+  return (
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={'Home'}/>
+          <Route path="/about" element={'About'}/>
+          <Route path="/projects" element={'Projcts'}/>
+          <Route path="/contact" element={'Contact'}/>
+          <Route path="/articles" element={'Articles'}/>
+          <Route path="/testimonials" element={'Testimonials'}/>
+        </Routes>
+      </Router>
+    </main>  
+  )
+}
+
+export default App
