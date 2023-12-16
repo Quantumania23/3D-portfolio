f4b13ccca30863b24b34d695b6116952384dd991
Mike Peace <mikepeace981@gmail.com>
2023-12-16 13:39:35 +0300

    update

--- text.js
+++ text.js
@@ -1,7 +1,10 @@
 npm i @react-three/fiber
-npm i @react-three/dei
+npm i @react-three/drei
 npm i react-router-dom
-
+npm i @react-spring/three
+npm i react-vertical-timeline-component
+npm i three
+npm i @emailjs/browser
 
 
 sketchFab : this is the place to get the 3d modules
